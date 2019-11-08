f0a92b15ae54c2681b7094523c1d656d2e3b08c7
Mark Tsujimura <masahiromarktsujimura@gmail.com>
2019-11-07 17:31:24 -0800

    init

---
+++ src/components/Layout.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import '../index.css';
+
+const Layout = ({ children }) => {
+    return (
+        <Fragment>
+            <main>{children}</main>
+            <Footer>
+                © {new Date().getFullYear()} Masahiro Mark Tsujimura
+            </Footer>
+        </Fragment>
+    );
+};
+
+Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+const Footer = styled.footer`
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    padding: 30px;
+    background-color: #3a435e;
+    color: #FBFBFB;
+`;
+
+export default Layout;
