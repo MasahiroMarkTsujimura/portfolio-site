bb959c994c1517eac740cf28c2574c50f72c3326
Masahiro Mark Tsujimura <masahiromarktsujimura@gmail.com>
2020-02-03 23:02:24 -0800

    more clean up

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,10 @@
 import React, { Fragment } from 'react';
-
 import SEO from '../components/Seo';
 
 const NotFoundPage = () => (
     <Fragment>
         <SEO title='404: Not found' />
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <h1>How did you get here?</h1>
     </Fragment>
 );
 
